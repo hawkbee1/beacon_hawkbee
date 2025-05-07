5ee8136ef1171a8430ace585a3f6d8864cd5de7e
Cirrus CI <support@cirruslabs.org>
2025-05-07 15:22:16 +0000

    init

---
+++ example/beacon_hawkbee_example.dart
@@ -0,0 +1,6 @@
+import 'package:beacon_hawkbee/beacon_hawkbee.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/src/beacon_hawkbee_base.dart
@@ -0,0 +1,70 @@
+/// Beacon is a protocol that enables communication between dApps and wallets.
+/// This library is a Dart implementation of the Beacon SDK.
+library beacon_hawkbee;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+
+part 'blockchain/blockchain.dart';
+part 'data/account.dart';
+part 'data/app_metadata.dart';
+part 'data/beacon_error.dart';
+part 'data/connection.dart';
+part 'data/network.dart';
+part 'data/peer.dart';
+part 'data/permission.dart';
+part 'data/signing_type.dart';
+part 'client/beacon_client.dart';
+part 'client/beacon_consumer.dart';
+part 'client/beacon_producer.dart';
+part 'configuration/log_level.dart';
+
+/// Core Beacon class that serves as the main entry point for the SDK.
+class Beacon {
+  /// The singleton instance of the Beacon SDK.
+  static Beacon? _instance;
+  
+  /// Gets the singleton instance of the Beacon SDK.
+  static Beacon get instance {
+    if (_instance == null) {
+      throw StateError('Beacon SDK not initialized. Call Beacon.init() first.');
+    }
+    return _instance!;
+  }
+  
+  /// Initializes the Beacon SDK.
+  static Future<Beacon> init({
+    required AppMetadata appMetadata,
+    LogLevel logLevel = LogLevel.none,
+  }) async {
+    if (_instance != null) {
+      return _instance!;
+    }
+    
+    _instance = Beacon._internal(
+      appMetadata: appMetadata,
+      logLevel: logLevel,
+    );
+    
+    await _instance!._initialize();
+    
+    return _instance!;
+  }
+  
+  /// The application metadata.
+  final AppMetadata appMetadata;
+  
+  /// The current log level.
+  final LogLevel logLevel;
+  
+  Beacon._internal({
+    required this.appMetadata,
+    required this.logLevel,
+  });
+  
+  Future<void> _initialize() async {
+    // Initialize core components
+    // This will be implemented later
+  }
+}
