248ea9e92fd21ebfdd0d2044eca63ced92b59859
Cirrus CI <support@cirruslabs.org>
2025-05-08 14:57:15 +0000

    iterate dart anaylze

--- lib/beacon_hawkbee.dart
+++ lib/beacon_hawkbee.dart
@@ -4,55 +4,24 @@
 /// allowing dApps to connect with wallets for blockchain operations.
 library beacon_hawkbee;
 
+// Export the base library that contains all the part files
 export 'src/beacon_hawkbee_base.dart';
 
-// Core data models
-export 'src/data/account.dart';
-export 'src/data/app_metadata.dart';
-export 'src/data/beacon_error.dart';
-export 'src/data/connection.dart';
-export 'src/data/network.dart';
-export 'src/data/peer.dart';
-export 'src/data/permission.dart';
-export 'src/data/signing_type.dart';
-
-// Client interfaces
-export 'src/client/beacon_client.dart';
-export 'src/client/beacon_consumer.dart';
-export 'src/client/beacon_producer.dart';
+// Client implementation files that are not parts
 export 'src/client/impl/client_factory.dart';
 export 'src/client/impl/dapp_client.dart';
 export 'src/client/impl/wallet_client.dart';
 
-// Configuration
-export 'src/configuration/log_level.dart';
-
-// Message types
-export 'src/message/beacon_message.dart';
-export 'src/message/disconnect_message.dart';
-export 'src/message/permission_request_message.dart';
-export 'src/message/permission_response_message.dart';
-
-// Blockchain abstractions
-export 'src/blockchain/blockchain.dart';
+// Blockchain implementations that are not parts
 export 'src/blockchain/tezos/tezos_blockchain.dart';
 export 'src/blockchain/tezos/message/tezos_operation_request.dart';
 
-// Storage
-export 'src/storage/storage_manager.dart';
-export 'src/storage/secure_storage.dart';
+// Storage implementations that are not parts
 export 'src/storage/impl/shared_preferences_storage.dart';
 export 'src/storage/impl/flutter_secure_storage_impl.dart';
 
-// Transport layer
-export 'src/transport/transport.dart';
-export 'src/transport/data/connection_message.dart';
-export 'src/transport/data/pairing_message.dart';
-export 'src/transport/p2p/p2p_client.dart';
-export 'src/transport/p2p/p2p_transport.dart';
+// Matrix implementation that is not a part
 export 'src/transport/p2p/matrix/matrix_client_provider.dart';
 
-// Cryptography
-export 'src/crypto/crypto_service.dart';
-export 'src/crypto/key_pair.dart';
+// Crypto implementation that is not a part
 export 'src/crypto/impl/sodium_crypto_service.dart';
